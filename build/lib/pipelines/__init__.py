340d5a591db4964c1f1a6918fe56b55ec7bca7da
Vu Quoc Hien <Neih4207@gmail.com>
2022-03-27 17:59:58 +0700

    remove build file

--- build/lib/pipelines/__init__.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import os
-
-# append parent directory to sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-def main():
-    print("HLA Prediction Tool")
--- build/lib/pipelines/evaluate.py
+++
@@ -1,4 +0,0 @@
-''' 
-    Author: Vu Quoc Hien
-    Date created: 2022-03-14
-'''
